45aa2168fc467fd4d5d6b32b7656505a689fd2df
chakradhar2203 <thathinenichakradhar@gmail.com>
2026-02-06 20:24:54 +0530

    reduced the header size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 
 export default function Navbar() {
     return (
-        <nav className="fixed top-0 left-0 right-0 z-50 h-[130px] backdrop-blur-xl bg-white/80 border-b border-gray-200/50">
+        <nav className="fixed top-0 left-0 right-0 z-50 h-[90px] backdrop-blur-xl bg-white/80 border-b border-gray-200/50">
             <div className="max-w-7xl mx-auto px-6 h-full flex items-center">
                 {/* Logo */}
                 <Link href="/" className="flex items-center group mr-auto">
                     {/* PNG Logo */}
-                    <div className="relative w-[140px] h-[140px] mt-2 transition-transform group-hover:scale-110">
+                    <div className="relative w-[100px] h-[100px] mt-2 transition-transform group-hover:scale-110">
                         <Image
                             src="/pic_logo_main_try.png"
                             alt="Andhra Avakai Logo"
